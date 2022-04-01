00794a36c425993c66070b847ddcaafc54743f1e
DimensionalDragon <andhikangurah@gmail.com>
2022-04-01 16:41:46 +0800

    Add index page for back end microservice projects

--- Back End Development and APIs/Certification API/server.js
+++ Back End Development and APIs/Certification API/server.js
@@ -16,41 +16,41 @@ db.once('open', () => console.log('Connected to database'));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Welcome!');
-})
+    res.sendFile(__dirname + '/views/index.html');
+});
 
 // Timestamp Microservice
 app.get('/timestamp', (req, res) => {
     res.sendFile(__dirname + '/views/timestamp.html');
-})
+});
 const timestampRouter = require('./routes/timestamp.js');
 app.use('/timestamp', timestampRouter);
 
 // Request Header Parser Microservice
 app.get('/header-parser', (req, res) => {
     res.sendFile(__dirname + '/views/header-parser.html');
-})
+});
 const headerParserRouter = require('./routes/headerParser.js');
 app.use('/header-parser', headerParserRouter);
 
 // URL Shortener Microservice
 app.get('/url-shortener', (req, res) => {
     res.sendFile(__dirname + '/views/url-shortener.html');
-})
+});
 const urlShortenerRouter = require('./routes/urlShortener.js');
 app.use('/url-shortener', urlShortenerRouter);
 
 // Exercise Tracker Microservice
 app.get('/exercise-tracker', (req, res) => {
     res.sendFile(__dirname + '/views/exercise-tracker.html');
-})
+});
 const exerciseTrackerRouter = require('./routes/exerciseTracker.js');
 app.use('/exercise-tracker', exerciseTrackerRouter);
 
 // File Metadata Microservice
 app.get('/file-metadata', (req, res) => {
     res.sendFile(__dirname + '/views/file-metadata.html');
-})
+});
 const fileMetadataRouter = require('./routes/fileMetadata.js');
 app.use('/file-metadata', fileMetadataRouter);
 
